e6358cb372f2ccd899f70da273c415bf18a18906
Ymx1ZQ <paolo.meola@gmail.com>
2019-04-05 21:59:03 +0200

    startup

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import './styles.scss'
+import Vue from 'vue'
+import App from './app.vue'
+
+const VueApp = new Vue({
+	el: '#app',
+	template: '<app/>',
+	components: {
+		App
+	}
+});
---
+++ src/app.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+    	{{message}}: {{counter}}
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue, {ComponentOptions} from 'vue'
+    import Component from 'vue-class-component'
+    import { Prop } from 'vue-property-decorator' 
+
+    @Component({
+        components: {
+
+        }
+    } as ComponentOptions<Vue>)
+    
+    export default class App extends Vue {
+        // props
+        // @Prop(String) readonly agencyId: string
+        // @Prop(String) readonly apiToken: string
+        
+        // state
+        message:string = "hello world"
+
+        // computed
+        get counter () {
+        	return this.message.length
+        }
+
+        // methods
+        mounted() {
+            //this.loadData()
+        }
+
+        
+    }
+</script>
+
+<style scoped lang="scss">
+    
+</style>
